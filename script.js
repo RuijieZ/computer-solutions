86d7c8e85e16d786cf6614f4f2735cf79cdbec2c
Ruijie Zhang <RuijieApplication@gmail.com>
2016-05-08 11:46:00 -0700

    finished the angular part

--- script.js
+++ script.js
@@ -17,15 +17,36 @@ app.config(['$routeProvider', function($routeProvider) {
 		})
 		.when("/contact", {
 			templateUrl: 'contact.html',
-			controller: 'mainController'
+			controller: 'contactController'
 		})
 		.when("/service", {
 			templateUrl: 'service.html',
-			controller: 'mainController'
-		})
-
+			controller: 'serviceController'
+		});
 }]);
 
 app.controller('mainController', ['$scope', function($scope) {
 	console.log("the mainController is running");
+}]);
+
+app.controller('serviceController', ['$scope','$http', function($scope, $http) {
+    console.log("the service controller is running");
+	$http.get('services.json')
+		.success(function(response) {
+           $scope.serviceData = response;
+		})
+		.error(function(response) {
+			console.log("there is an error in loading service.json file");
+		})   
+}]);
+
+app.controller('contactController', ['$scope', '$http', function($scope, $http) {
+    console.log('contactController is running');
+    $http.get('location.json')
+        .success(function(response){
+            $scope.locations = response;
+        })
+        .error(function(response){
+            console.log("there is an error in getting data from location.json");
+        })
 }]);
