f401429813f709cde5d598ddedd03d947ea1b854
Ruijie Zhang <RuijieApplication@gmail.com>
2016-05-07 20:46:03 -0700

    basic angular route function working

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-var app
+var app = angular.module('computer', ['ngRoute']);
+
+
+app.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when("/", {
+			templateUrl: 'main.html',
+			controller:'mainController'
+		})
+		.when("/about", {
+			templateUrl: 'about.html',
+			controller: 'aboutController'
+		})
+}]);
+
+app.controller('mainController', ['$scope', function($scope) {
+	console.log("the mainController is running");
+
+}]);
+
+app.controller('aboutController', ['$scope', function($scope) {
+
+}]);
